4061d2776c0ae899c35e7d12b6ad3aaee16d5d8a
SashaRoket16 <106405347+SashaRoket16@users.noreply.github.com>
2022-10-04 14:11:43 +0300

    Add files via upload

---
+++ src/ProductPage.js
@@ -0,0 +1,3 @@
+import user from "./shorts.js";
+console.log("ff")
+console.log(user)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+let inputSearch = document.querySelector(".input1")
+inputSearch.addEventListener("click", (e)=>{
+    window.open("./shorts.html");
+
+
+
+})
+import user from "./shorts.js"
+console.log(user)
